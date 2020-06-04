4a3d4491d0feb3ed3ae8d14256c37ed446acf29c
jwstegemann <jwstegemann@gmail.com>
2020-06-04 13:12:53 +0200

    working example with mpp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,47 @@
 buildscript {
     repositories {
-        mavenLocal()
-        mavenCentral()
+        mavenLocal() //FIXME: nicht einchecken
         jcenter()
     }
 
     dependencies {
         classpath(kotlin("gradle-plugin"))
+        classpath("io.fritz2:fritz2-gradle-plugin:0.5")
     }
 }
 
 plugins {
-    kotlin("js") version("1.3.71")
+    kotlin("multiplatform") version("1.3.72")
+    kotlin("kapt") version("1.3.72")
 }
 
-kotlin {
-    target {
-        browser {
-        }
-    }
-}
+apply(plugin = "io.fritz2.fritz2-gradle")
 
 repositories {
+    mavenLocal()
     jcenter()
 }
 
-dependencies {
-    implementation(kotlin("stdlib-js"))
-    implementation("io.fritz2:fritz2-core-js:0.4")
-}
+kotlin {
+    kotlin {
+        jvm()
+        js().browser()
 
+        sourceSets {
+            val commonMain by getting {
+                dependencies {
+                    implementation(kotlin("stdlib"))
+                }
+            }
+            val jvmMain by getting {
+                dependencies {
+                }
+            }
+            val jsMain by getting {
+                dependencies {
+                }
+            }
+
+        }
+    }
+}
---
+++ src/commonMain/kotlin/model.kt
@@ -0,0 +1,4 @@
+import io.fritz2.lenses.Lenses
+
+@Lenses
+data class Person(val name: String, val age: Int)
