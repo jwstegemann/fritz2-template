378dac8fb2e3b351c98852ccc1791af320e1a300
jan <jan@rexster>
2021-03-02 18:32:06 +0100

    release 0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,22 @@
 plugins {
-    id("dev.fritz2.fritz2-gradle") version "0.9-SNAPSHOT"
+    id("dev.fritz2.fritz2-gradle") version "0.9"
 }
 
 repositories {
     jcenter()
     maven("https://dl.bintray.com/jwstegemann/fritz2")
-    maven("https://oss.jfrog.org/artifactory/jfrog-dependencies")
 }
 
 kotlin {
     jvm()
-//        js().browser()  // working
-    js(IR) {      // not working
+    js(IR) {
         browser()
     }.binaries.executable()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("dev.fritz2:core:0.9-SNAPSHOT")
+                implementation("dev.fritz2:core:0.9")
             }
         }
         val jvmMain by getting {
