8120ff189a303ac9606f015ef9c157fe59e280c9
jwstegemann <jwstegemann@gmail.com>
2021-11-19 21:32:09 +0100

    working again...

--- src/jsMain/kotlin/app.kt
+++ src/jsMain/kotlin/app.kt
@@ -1,11 +1,10 @@
 import dev.fritz2.binding.storeOf
-import dev.fritz2.dom.DomLifecycleHandler
-import dev.fritz2.dom.WithDomNode
+import dev.fritz2.dom.*
 import dev.fritz2.dom.html.RenderContext
 import dev.fritz2.dom.html.Scope
 import dev.fritz2.dom.html.ScopeContext
 import dev.fritz2.dom.html.render
-import dev.fritz2.dom.mountPoint
+import dev.fritz2.identification.Id
 import kotlinx.browser.document
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.asDeferred
@@ -21,7 +20,38 @@ class Transition(
     val leave: String? = null,
     val leaveStart: String? = null,
     val leaveEnd: String? = null
-)
+) {
+    companion object {
+        val leaveTransition: DomLifecycleHandler = { target, payload ->
+            val transition = payload.unsafeCast<Transition?>()
+            if (transition?.leave != null) {
+                val classes = target.domNode.getAttribute("class").orEmpty()
+                target.domNode.setAttribute("class", "$classes ${transition.leaveStart.orEmpty()}")
+                target.domNode.setAttribute("class", "$classes ${transition.leave} ${transition.leaveEnd.orEmpty()}")
+                animationDone(target.domNode).asDeferred()
+            } else null
+        }
+
+        val enterTransition: DomLifecycleHandler = { target, payload ->
+            val transition = payload.unsafeCast<Transition?>()
+            if (transition?.enter != null) {
+                val classes = target.domNode.getAttribute("class").orEmpty()
+                target.domNode.setAttribute("class", "$classes ${transition.enterStart.orEmpty()}")
+                //TODO: is this needed a second time in some browsers?
+                kotlinx.browser.window.requestAnimationFrame {
+                    target.domNode.setAttribute(
+                        "class",
+                        "$classes ${transition.enter} ${transition.enterEnd.orEmpty()}"
+                    )
+                    animationDone(target.domNode).then {
+                        target.domNode.setAttribute("class", classes)
+                    }
+                }
+            }
+            null
+        }
+    }
+}
 
 val fade = Transition(
     "transition-all duration-1000",
@@ -48,31 +78,6 @@ val animationDone = nativeFunction<(Node) -> Promise<Unit>>(
     """.trimIndent()
 )
 
-val leaveTransition: DomLifecycleHandler = { target, payload ->
-    val transition = payload.unsafeCast<Transition?>()
-    if (transition?.leave != null) {
-        val classes = target.domNode.getAttribute("class").orEmpty()
-        target.domNode.setAttribute("class", "$classes ${transition.leaveStart.orEmpty()}")
-        target.domNode.setAttribute("class", "$classes ${transition.leave} ${transition.leaveEnd.orEmpty()}")
-        animationDone(target.domNode).asDeferred()
-    } else null
-}
-
-val enterTransition: DomLifecycleHandler = { target, payload ->
-    val transition = payload.unsafeCast<Transition?>()
-    if (transition?.enter != null) {
-        val classes = target.domNode.getAttribute("class").orEmpty()
-        target.domNode.setAttribute("class", "$classes ${transition.enterStart.orEmpty()}")
-        //TODO: is this needed a second time in some browsers?
-        kotlinx.browser.window.requestAnimationFrame {
-            target.domNode.setAttribute("class", "$classes ${transition.enter} ${transition.enterEnd.orEmpty()}")
-            animationDone(target.domNode).then {
-                target.domNode.setAttribute("class", classes)
-            }
-        }
-    }
-    null
-}
 
 class Fragment(override val job: Job, override val scope: Scope, private val apply: WithDomNode<Element>.() -> Unit) :
     RenderContext, WithDomNode<DocumentFragment> {
@@ -89,7 +94,7 @@ class Fragment(override val job: Job, override val scope: Scope, private val app
 fun RenderContext.fragment(
     scopeContext: (ScopeContext.() -> Unit) = {},
     content: RenderContext.() -> Unit,
-    apply: WithDomNode<Element>.() -> Unit,
+    apply: WithDomNode<Element>.() -> Unit = {},
 ): Fragment {
     //FIXME: replace by evalScope
     val s = ScopeContext(this.scope).apply(scopeContext).scope
@@ -97,12 +102,10 @@ fun RenderContext.fragment(
 }
 
 
-fun RenderContext.transition(transition: Transition, content: RenderContext.() -> Unit) =
-    fragment(content = content) {
-        mountPoint()?.apply {
-            beforeUnmount(this@fragment, leaveTransition, fade)
-            afterMount(this@fragment, enterTransition, fade)
-        }
+fun RenderContext.transition(transition: Transition, content: RenderContext.() -> Tag<*>) =
+    content().apply {
+        if (transition.leave != null) beforeUnmount(transition, Transition.leaveTransition)
+        if (transition.enter != null) afterMount(transition, Transition.enterTransition)
     }
 
 fun RenderContext.transition(
@@ -111,15 +114,48 @@ fun RenderContext.transition(
     enterEnd: String? = null,
     leave: String? = null,
     leaveStart: String? = null,
-    leaveEnd: String? = null, content: RenderContext.() -> Unit
+    leaveEnd: String? = null, content: RenderContext.() -> Tag<*>
 ) = transition(Transition(enter, enterStart, enterEnd, leave, leaveStart, leaveEnd), content)
 
 
 fun main() {
     val visible = storeOf(true)
 
+    val list = storeOf(listOf(Id.next(), Id.next(), Id.next()))
+    val addItem = list.handle { it + Id.next() }
+    val addMany = list.handle { it + Id.next() + Id.next() + Id.next() }
+    val removeItem = list.handle { list, value: String -> list - value }
+
     render {
         div {
+            button("p-4 border") {
+                +"add"
+                clicks handledBy addItem
+            }
+            button("p-4 border") {
+                +"add many"
+                clicks handledBy addMany
+            }
+            button("p-4 border") {
+                +"shuffle"
+                clicks handledBy list.handle { it.shuffled() }
+            }
+
+            list.data.renderEach({ it }) { item ->
+                div { //FIXME: get rid of this?
+                    transition(fade) {
+                        div(id = "myDiv") {
+                            inlineStyle("margin-top: 10px; width: 200px; height: 50px; background-color: lightblue;")
+                            +item
+                            clicks.map { item } handledBy removeItem
+                        }
+                    }
+                }
+            }
+        }
+
+        div {
+            inlineStyle("margin-top: 20px;")
             button("p-4 border") {
                 +"toggle"
                 clicks handledBy visible.handle { !it }
@@ -127,16 +163,18 @@ fun main() {
             visible.data.render {
                 if (it) {
                     transition(fade) {
-                        div(id = "myDiv") {
-                            inlineStyle("margin-top: 10px; width: 200px; height: 200px; background-color: red;")
-                        }
+                        div {
+                            div(id = "myDiv") {
+                                inlineStyle("margin-top: 10px; width: 200px; height: 200px; background-color: red;")
+                            }
 
-                        div(id = "myDiv2") {
-                            inlineStyle("margin-top: 10px; width: 200px; height: 200px; background-color: red;")
-                        }
+                            div(id = "myDiv2") {
+                                inlineStyle("margin-top: 10px; width: 200px; height: 200px; background-color: red;")
+                            }
 
-                        div(id = "myDiv3") {
-                            inlineStyle("margin-top: 10px; width: 200px; height: 200px; background-color: red;")
+                            div(id = "myDiv3") {
+                                inlineStyle("margin-top: 10px; width: 200px; height: 200px; background-color: red;")
+                            }
                         }
                     }
                 }
