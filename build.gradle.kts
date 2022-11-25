4243db1f8af08c6b52d879653935ef380622b1a5
jwstegemann <jwstegemann@gmail.com>
2022-11-25 16:17:10 +0100

    set fritz-version to 1.0-RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") // new repository here
 }
 
-val fritz2Version = "1.0-SNAPSHOT"
+val fritz2Version = "1.0-RC2"
 
 //group = "my.fritz2.app"
 //version = "0.0.1-SNAPSHOT"
