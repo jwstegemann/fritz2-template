48fd600e9132e469e7bf2e986417057c4bc195ff
jamowei <jan@rexster.de>
2023-08-01 15:41:25 +0200

    Bump fritz2 version to `1.0-RC7`

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") // new repository here
 }
 
-val fritz2Version = "1.0-RC6"
+val fritz2Version = "1.0-RC7"
 
 //group = "my.fritz2.app"
 //version = "0.0.1-SNAPSHOT"
